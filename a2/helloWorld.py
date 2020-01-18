0dcaacdfeeacc219f429b810a944ccdf7f6b1794
Sebastian <sebastian@feldam.com>
2020-01-17 23:33:32 -0500

    Create helloWorld.py

---
+++ a2/helloWorld.py
@@ -0,0 +1,14 @@
+#author: Sebastian V. Feldam
+#date: 1.17.20
+
+#variable
+myName = 'Sebastian'
+age = 21
+country = 'DK'
+height = 194
+
+#print statement
+print('-  Hello world, I am ' + myName)
+print('-  I am ' + str(age) + ' years')
+print('-  I am from ' + country)
+print('-  I am ' + str(height) + 'cm tall')
